eab605854dc2acd6df6b261449012b842ffaee09
RyanMKrol <ryankrol.m@gmail.com>
2024-09-21 19:06:32 +0100

    Add caching to stop fetching the entire library every time

--- src/remote/index.js
+++ src/remote/index.js
@@ -4,6 +4,8 @@ import Show from '../model/Show';
 import Season from '../model/Season';
 import Episode from '../model/Episode';
 
+import { getCachedData, getCacheLocWithFilename } from '../utils/StoredCache';
+
 import 'dotenv/config';
 /**
  * Fetch the raw library data for my "Movies" library
@@ -18,21 +20,32 @@ async function fetchRawPlexMovieLibraryData() {
  * @returns {Array<object>} An array of plex library items
  */
 async function fetchRawPlexTelevisionLibraryData() {
-  const rawTelevisionLibraryData = await fetchLocalXmlData(`https://192.168.1.130:32400/library/sections/5/all?X-Plex-Token=${process.env.PLEX_API_TOKEN}`);
+  const rawTelevisionLibraryData = await getCachedData(
+    getCacheLocWithFilename('shows'),
+    async () => fetchLocalXmlData(`https://192.168.1.130:32400/library/sections/5/all?X-Plex-Token=${process.env.PLEX_API_TOKEN}`),
+  );
 
   return Promise.all(
     rawTelevisionLibraryData.MediaContainer.Directory
       .map(async (item) => {
         const show = new Show(item);
 
-        const seasonsData = await fetchUnspecifiedPlexData(show.childrenUri);
+        const seasonsData = await getCachedData(
+          getCacheLocWithFilename(show.title),
+          async () => fetchUnspecifiedPlexData(show.childrenUri),
+        );
+
         const dir = Array.isArray(seasonsData.MediaContainer.Directory)
           ? seasonsData.MediaContainer.Directory
           : [seasonsData.MediaContainer.Directory];
 
         const seasons = await Promise.all(dir.map(async (seasonData) => {
           const season = new Season(seasonData);
-          const episodesDataRaw = await fetchUnspecifiedPlexData(season.childrenUri);
+          const episodesDataRaw = await getCachedData(
+            getCacheLocWithFilename(`${show.title}_${season.title}`),
+            async () => fetchUnspecifiedPlexData(season.childrenUri),
+          );
+
           const episodesData = Array.isArray(episodesDataRaw.MediaContainer.Video)
             ? episodesDataRaw.MediaContainer.Video
             : [episodesDataRaw.MediaContainer.Video];
---
+++ src/utils/StoredCache.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-underscore-dangle */
+
+import fs from 'fs';
+
+/**
+ * test
+ * @param {string} filename The name of the file to store
+ * @returns {string} Location of the tv cache
+ */
+export function getCacheLocWithFilename(filename) {
+  return `src/resources/${filename}.json`;
+}
+
+/**
+ * Checks or creates cache data
+ * @param {string} location location of the file to cache
+ * @param {Function} populateCacheFn how to create new data for the cache
+ * @returns {any} Whatever you've populated in the cache
+ */
+export async function getCachedData(location, populateCacheFn) {
+  if (fs.existsSync(location)) {
+    const rawCacheData = fs.readFileSync(location, { encoding: 'utf8' });
+
+    const data = JSON.parse(rawCacheData);
+    const { cacheTtl, val } = data;
+
+    if (typeof cacheTtl === 'undefined' || typeof data === 'undefined') {
+      throw new Error('Cache is not in expected format');
+    }
+
+    if (new Date() < new Date(cacheTtl)) {
+      return val;
+    }
+    console.log('could not use the cached value, moving on!');
+  }
+
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
+  const newCacheData = await populateCacheFn();
+
+  const jsonString = JSON.stringify({ cacheTtl: tomorrow, val: newCacheData }, null, 2);
+
+  fs.writeFileSync(location, jsonString, { flag: 'w' });
+
+  return newCacheData;
+}
